2243ca3d08c11d5f4224c061c9234514a0586513
victor correa <vcorrea@medianet.com.ve>
2023-08-07 12:27:00 -0400

    search

--- web/pages/search.tsx
+++ web/pages/search.tsx
@@ -10,14 +10,28 @@ export const Search =() =>{
             console.log(id) 
             var id= id
             document.getElementById(id).classList.toggle("bi-star"); 
-    };
+    }; 
+    
+      const [filteredList, setFilteredList] = useState(usersU);
+      const filterBySearch = (event) => {
+        // Access input value
+        const query = event.target.value;
+        // Create copy of item list
+        var updatedList = [...usersU];
+        // Include all elements which includes the search query
+        updatedList = updatedList.filter((item) => {
+          return item.name.toString().toLowerCase().indexOf(query.toLowerCase()) !== -1;
+        });
+        // Trigger render with updated values
+        setFilteredList(updatedList);
+      };
     const show = (event) => {
         navigate('/profile');
     }; 
     useEffect(() => {
         axios.get("http://localhost:3000/universities").then((resp) => {
             console.log(resp.data); 
-            setUsers(resp.data)
+            setUsers(resp.data) 
         });  
     }, [])
     return (
@@ -30,12 +44,13 @@ export const Search =() =>{
                     placeholder="Search"
                     className="me-2 inputSearch"
                     aria-label="Search"
+                    onChange={filterBySearch} 
                     />
                     <Button className='btnSearch'>
                         <i className="bi bi-search"></i> 
                     </Button>
-                </Form>
-                        {usersU.map(uni => (
+                </Form> 
+                        {filteredList.map(uni => (
                             <Card className='shadow relative boxFavorite' key={uni.id}>
                               <Card.Body> 
                                   <Card.Title>{uni.name} <span className='country'> {uni.Country}</span> 
